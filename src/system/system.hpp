ba62b1e9d655a968e50784e71728e3a7c8b3f064
Mark <mtik.philosopher@gmail.com>
2025-07-20 13:41:39 -0700

    doxygen fix

--- src/system/system.hpp
+++ src/system/system.hpp
@@ -1,9 +1,13 @@
 /**
 * @file system.hpp
 *
+* @brief Module header for the etask system management module.
+*
+* @defgroup etask_system etask::system
+*
 * @author Mark Tikhonov <mtik.philosopher@gmail.com>
 *
-* @date 2025-07-09
+* @date 2025-07-20
 *
 * @copyright
 * Business Source License 1.1 (BSL 1.1)
@@ -13,9 +17,7 @@
 */
 #ifndef ETASK_SYSTEM_SYSTEM_HPP_
 #define ETASK_SYSTEM_SYSTEM_HPP_
-
 #include "status_code.hpp"
 #include "tasks/tasks.hpp"
 #include "managment/management.hpp"
-
 #endif // ETASK_SYSTEM_SYSTEM_HPP_
